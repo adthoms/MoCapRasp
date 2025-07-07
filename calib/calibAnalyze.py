c0590d51cd3a9d25ef51272fb309ca43a4696ea8
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2025-07-07 14:34:09 -0700

    Refactor calibration scripts for improved readability and consistency in variable naming

--- calib/calibAnalyze.py
+++ calib/calibAnalyze.py
@@ -3,14 +3,16 @@ import numpy as np
 import glob
 
 # Chessboard size
-checkerboard_size = (9,9)  # inner corners (adapt if your board is different)
+checkerboard_size = (9, 9)  # inner corners (adapt if your board is different)
 
 # prepare object points
-objp = np.zeros((checkerboard_size[0]*checkerboard_size[1],3), np.float32)
-objp[:,:2] = np.mgrid[0:checkerboard_size[0],0:checkerboard_size[1]].T.reshape(-1,2)
+objp = np.zeros((checkerboard_size[0] * checkerboard_size[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0 : checkerboard_size[0], 0 : checkerboard_size[1]].T.reshape(
+    -1, 2
+)
 
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
 
 # load images
 images = glob.glob(os.path.expanduser("~/calibration_images/*.jpg"))
@@ -27,13 +29,15 @@ for fname in images:
 
         # Optional: draw and show the corners
         cv2.drawChessboardCorners(img, checkerboard_size, corners, ret)
-        cv2.imshow('img', img)
+        cv2.imshow("img", img)
         cv2.waitKey(100)
 
 cv2.destroyAllWindows()
 
 # Camera calibration
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+    objpoints, imgpoints, gray.shape[::-1], None, None
+)
 
 print("Camera matrix : \n")
 print(mtx)
